8921a8ebde4aac85828ef7e47db6745202fdebed
maevelee <rura.d.lee@gmail.com>
2021-04-17 02:01:24 +0900

    LISP

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "블로그 시작",
+        "excerpt":"남들 다 하는 블로그 나도 만들었다.   세팅하면서 tzinfo 에러 떠서 화났는데 막상 만들고 나니까 엄청 뿌듯하다!!!!!   이런 블로그 형식은 처음 경험해보는데 티스토리나 브런치보다 간결하고 또 변경 사항이 생길때마다 terminal에서 수정해야 하는게 아날로그 감성이라 마음에 든다.   앞으로 천천히 공부한 거 정리해 올려야징  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "https://maevelee.github.io//blog/first-post/",
+        "teaser": "https://maevelee.github.io/"
+      },{
+        "title": "CS50 4주차 - 배열",
+        "excerpt":"##배열, 알고리즘과 알고리즘 실행 시간 표기법 배열이란 한 자료형의 여러 값들이 메모리상에 모여 있는 구조이다. 현실 세계로 비유하자면 학창시절 교실 뒤에 있던 사물함이다. 컴퓨터의 메모리는 작은 사물함들이 칸칸이 나열된 모습이다. 사람이라면 이 사물함 안에 들어있는 것을 훑어내서 전체를 파악할 수 있지만 컴퓨터는 그런 능력이 없다. 컴퓨터는 값들에 접근할 때 배열의...","categories": ["TIL"],
+        "tags": ["CS50"],
+        "url": "https://maevelee.github.io//til/cs50-week4-1/",
+        "teaser": "https://maevelee.github.io/"
+      },{
+        "title": "CS50 4주차 - 선형 검색, 이진 검색",
+        "excerpt":"검색 알고리즘인 선형검색, 이진검색을 알아보자. 이전 포스팅에서 알고리즘 실행 시간 표기법을 다뤘다. 그렇다면 어떤 값이 배열 안에 속해 있는지 찾아볼 수 있는 검색 알고리즘에는 무엇이 있을까? 어떤 알고리즘이 어떠한 상황에 쓰이고, 가장 효율적인 알고리즘은 어떤 것일까? 어떤 값이 배열 안에 속해 있는지 찾아보기 위해서는 크게 두 가지 검색 알고리즘을 사용할...","categories": ["TIL"],
+        "tags": ["CS50"],
+        "url": "https://maevelee.github.io//til/cs50-week4-2/",
+        "teaser": "https://maevelee.github.io/"
+      }]
